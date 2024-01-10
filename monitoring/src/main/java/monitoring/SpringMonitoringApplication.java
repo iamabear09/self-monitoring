e547e8b87519c4d8bd5a1d6657e905f802dd5f7b
jonghun Park <jhp2014.dev@gmail.com>
2024-01-11 02:58:59 +0900

    #1:refator rename SpringApp

--- monitoring/src/main/java/monitoring/SpringMonitoringApplication.java
+++ monitoring/src/main/java/monitoring/SpringMonitoringApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MonitoringApplication {
+public class SpringMonitoringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MonitoringApplication.class, args);
+        SpringApplication.run(SpringMonitoringApplication.class, args);
     }
 }
