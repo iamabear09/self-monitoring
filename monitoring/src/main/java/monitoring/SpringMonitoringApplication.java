8938c5e6b9b60cf7af1babd8cc6397e7f7c63756
iamabear09 <151811667+iamabear09@users.noreply.github.com>
2024-01-11 19:39:15 +0900

    #1 project setting (#4)
    * #1 gradle init
    
    add .idea in .gitignore file
    
    * #1 multi module structure setting
    
    add module monitoring
    write build.gradle & setting.gradle
    
    * #1:refator rename SpringApp
    
    * #1 chore
    
    - delete unessecary comment in setting.gradle
    - change  to '' in build.gradle

---
+++ monitoring/src/main/java/monitoring/SpringMonitoringApplication.java
@@ -0,0 +1,12 @@
+package monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMonitoringApplication.class, args);
+    }
+}
