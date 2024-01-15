70acaa3a4dc478827978178be480f99f1e67e7c3
jonghun Park <jhp2014.dev@gmail.com>
2024-01-15 19:51:57 +0900

    #3 refactor: api create record
    - change record api format
    - now record dto can have multiple time records

--- monitoring/src/main/java/monitoring/api/dto/RecordDto.java
+++ monitoring/src/main/java/monitoring/api/dto/RecordDto.java
@@ -4,27 +4,48 @@ import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 @Getter
-@EqualsAndHashCode //for test
-@ToString
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode  //for test
+@ToString   //for test
 public class RecordDto {
 
     private Long recordId;
-    private LocalDate date;
-    private LocalTime startTime;
-    private Long durationMinutes;
     private String action;
     private String memo;
 
+    private Integer timeRecordsNum;
+    private List<Time> timeRecords;
+
     @Builder
-    private RecordDto(Long recordId, LocalDate date, LocalTime startTime, Long durationMinutes, String action, String memo) {
+    private RecordDto(Long recordId, String action, String memo, List<Time> timeRecords) {
         this.recordId = recordId;
-        this.date = date;
-        this.startTime = startTime;
-        this.durationMinutes = durationMinutes;
         this.action = action;
         this.memo = memo;
+        this.timeRecordsNum = timeRecords.size();
+        this.timeRecords = timeRecords;
+    }
+
+
+    // To save time records
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class Time {
+        private Long timeId;
+        private LocalDate date;
+        private LocalTime startTime;
+        private LocalTime endTime;
+        private Long durationMinutes;
+
+        @Builder
+        private Time(Long timeId, LocalDate date, LocalTime startTime, Long durationMinutes) {
+            this.timeId = timeId;
+            this.date = date;
+            this.startTime = startTime;
+            this.endTime = startTime.plusMinutes(durationMinutes);
+            this.durationMinutes = durationMinutes;
+        }
     }
 }
