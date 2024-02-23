5a12454a13a5ba14b8513e4ab412ab69fe136a22
jonghun Park <jhp2014.dev@gmail.com>
2024-02-23 14:55:46 +0900

    #14 refactor: EOF 추가 및 주석 변경

--- monitoring/docker-compose/docker-init.sh
+++ monitoring/docker-compose/docker-init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# 로컬에서 모니터링 환경과 부하 테스트 환경 설정을 위해 한번에 필요한 Container Up
+
 composeDir="."
 
 # mysql up
@@ -10,5 +12,4 @@ docker-compose -f ${composeDir}/docker-compose-monitoring.yml up -d
 docker-compose -f ${composeDir}/docker-compose-scouter.yml up -d
 # load test
 docker-compose -f ${composeDir}/docker-compose-k6.yml up influxdb test-dashboard -d
-
 
